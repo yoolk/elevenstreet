a80328891d3077f8f005c5ec73a42f698931fefb
Rothana Choun <rothana.choun7@gmail.com>
2016-03-10 16:52:38 +0700

    fix(): response category list

--- lib/elevenstreet/api/category.rb
+++ lib/elevenstreet/api/category.rb
@@ -4,13 +4,13 @@ module Elevenstreet
       def categories
         response = self.class.get('/cateservice/category', headers: headers)
 
-        response['categorys']['category']
+        response['categorys']['category'] if response['categorys'].present?
       end
 
       def sub_categories(parent_category_id)
         response = self.class.get("/cateservice/category/#{parent_category_id}", headers: headers)
 
-        response['categorys']['category']
+        response['categorys']['category'] if response['categorys'].present?
       end
     end
   end
